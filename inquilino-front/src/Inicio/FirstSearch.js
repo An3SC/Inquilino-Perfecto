07bd30a3e464ea8fa409d395fb8d1a896576cc78
An3SC <asierracardalda@outlook.es>
2021-02-05 11:45:48 +0100

    creadas las funciones de FirstSearch y SearchPage

---
+++ inquilino-front/src/Inicio/FirstSearch.js
@@ -0,0 +1,48 @@
+import { useState } from "react"
+import { useHistory } from "react-router-dom"
+
+function FirstSearch() {
+    const [city, setCity] = useState('')
+    const [price, setPrice] = useState('')
+
+    const [results, setResults] = useState()
+
+    let history = useHistory()
+
+    const handleSubmit = async e => {
+        e.preventDefault()
+        console.log(city, price)
+        const url = `http://localhost:9999/vivienda/busqueda?` + `ciudad=${city}` + `&precio2=${price}`
+        const res = await fetch(url)
+        const data = await res.json()
+        console.log(data)
+        setResults(data)
+        history.push('/search')
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    Dinos dónde buscas
+                </div>
+                <input type='text' placeholder='Ciudad...'
+                    value={city} onChange={e => setCity(e.target.value)}
+                />
+                <label>
+                    Precio máximo
+                    <select value={price} onChange={e => setPrice(e.target.value)}>
+                        <option value='' hidden>Max</option>
+                        <option value={400}>400</option>
+                        <option value={500}>500</option>
+                        <option value={600}>600</option>
+                        <option value={700}>700</option>
+                    </select>
+                </label>
+                <button>Buscar</button>
+            </form>
+        </div>
+    )
+}
+
+export default FirstSearch
---
+++ inquilino-front/src/Viviendas/SearchPage.js
@@ -0,0 +1,7 @@
+function SearchPage() {
+    return (
+        <div>Hola</div>
+    )
+}
+
+export default SearchPage 
