f8695a0d79e1939ca87135a60970f2c56d8af7ec
An3SC <asierracardalda@outlook.es>
2021-02-11 12:21:17 +0100

    solucionada la primera busqueda por params y la busqueda multicampo completa

--- inquilino-front/src/Inicio/FirstSearch.js
+++ inquilino-front/src/Inicio/FirstSearch.js
@@ -2,43 +2,24 @@ import { useState } from "react"
 import { useHistory } from "react-router-dom"
 
 function FirstSearch() {
-    const [city, setCity] = useState('')
-    // const [price, setPrice] = useState('')
-    // const [banos, setBanos] = useState('')
+    const [cityUrl, setCityUrl] = useState('')
 
     let history = useHistory()
 
-    // FUNCIÓN CON USO DE PARAMS
-    // const handleSubmit = async e => {
-    //     e.preventDefault()
-    //     history.push(`/search/${city}/${banos}`)
-    // }
-
     const handleSubmit = async e => {
         e.preventDefault()
-        history.push(`/search/&ciudad=${city}`)
+        history.push(`/search/${cityUrl}`)
     }
 
     return (
         <div>
             <form onSubmit={handleSubmit}>
                 <div>
-                    Dinos dónde buscas
+                    🦕🦖 dónde buscas
                 </div>
                 <input type='text' placeholder='Ciudad...'
-                    value={city} onChange={e => setCity(e.target.value)}
+                    value={cityUrl} onChange={e => setCityUrl(e.target.value)}
                 />
-                {/* <input type='number' min='0' value={banos} onChange={e => setBanos(e.target.value)} /> */}
-                {/* <label>
-                    Precio máximo
-                    <select value={price} onChange={e => setPrice(e.target.value)}>
-                        <option value='' hidden>Max</option>
-                        <option value={400}>400</option>
-                        <option value={500}>500</option>
-                        <option value={600}>600</option>
-                        <option value={700}>700</option>
-                    </select>
-                </label> */}
                 <button>Buscar</button>
             </form>
         </div>
--- inquilino-front/src/Viviendas/SearchPage.js
+++ inquilino-front/src/Viviendas/SearchPage.js
@@ -1,71 +1,143 @@
 import { useState } from 'react'
-// import { useParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 import useFetch from '../useFetch'
 
 function SearchPage() {
     const [city, setCity] = useState('')
-    const [price1, setPrice1] = useState('')
-    const [price2, setPrice2] = useState('')
+    const [provincia, setProvincia] = useState('')
+    const [nHabitaciones, setNHabitaciones] = useState('')
+    const [m2, setM2] = useState('')
+    const [nBanos, setNBanos] = useState('')
+    const [precio1, setPrecio1] = useState('')
+    const [precio2, setPrecio2] = useState('')
+    const [ascensor, setAscensor] = useState(false)
+    const [garaje, setGaraje] = useState(false)
+    const [balcon, setBalcon] = useState(false)
+    const [jardin, setJardin] = useState(false)
 
+    const [results, setResults] = useState('')
+    const dataResults = results.data
 
-    const currentUrl = window.location.href
-    const currentCity = currentUrl && currentUrl.split('&').find(t => t.startsWith('ciudad='))
-    // const currentBanos = currentUrl && currentUrl.split('&').find(t => t.startsWith('nBanos='))
-    // const currentPrice = currentUrl && currentUrl.split('&').find(t => t.startsWith('price='))
-    const cityUrl = currentCity ? currentCity.substr(7) : 1
-    // const price = currentPrice ? currentPrice.substr(5) : 1
-
-    // console.log(currentBanos)
-
-    // FUNCIÓN CON USO DE PARAMS
-    // const { city, banos } = useParams()
+    const { cityUrl } = useParams()
 
     const searchPage = useFetch(`http://localhost:9999/vivienda/busqueda?` + `ciudad=${cityUrl}`) || []
     const firstResults = searchPage.data
-    // console.log(firstResults)
 
     const handleSubmit = async e => {
         e.preventDefault()
-        //     const url = `http://localhost:9999/vivienda/busqueda?` + `ciudad=${city}` /*+ `&precio2=${price}`*/
-        //     const res = await fetch(url)
-        //     const data = await res.json()
-        //     setResults(data)
-        //     // history.push(`/search/&ciudad=${city}`)
+        const url = `http://localhost:9999/vivienda/busqueda?`
+            + `ciudad=${city ? city : cityUrl}` + `&provincia=${provincia}` + `&nBanos=${nBanos}`
+            + `&nHabitaciones=${nHabitaciones}` + `&m2=${m2}` + `&precio1=${precio1}`
+            + `&precio2=${precio2}` + `&ascensor=${ascensor ? 'si' : ''}` + `&garaje=${garaje ? 'si' : ''}`
+            + `&balcon=${balcon ? 'si' : ''}` + `&jardin=${jardin ? 'si' : ''}`
+        const res = await fetch(url)
+        const data = await res.json()
+        setResults(data)
     }
+
+    const handleReset = e => {
+        e.preventDefault()
+        setCity('')
+        setProvincia('')
+        setNBanos('')
+        setNHabitaciones('')
+        setM2('')
+        setPrecio1('')
+        setPrecio2('')
+        setAscensor('')
+        setGaraje('')
+        setBalcon('')
+        setJardin('')
+    }
+
+    console.log(results)
 
     return (
         <div>
-            <form onSubmit={handleSubmit}>
-                <input name='ciudad' placeholder='Ciudad...' value={city} onChange={e => setCity(e.target.value)} />
-                <label>
-                    Precio
-                    <select value={price1} onChange={e => setPrice1(e.target.value)}>
-                        <option value='' hidden>Min</option>
-                        <option value={200}>400</option>
-                        <option value={300}>500</option>
-                        <option value={400}>600</option>
-                        <option value={500}>700</option>
-                    </select>
-                    <select value={price2} onChange={e => setPrice2(e.target.value)}>
-                        <option value='' hidden>Max</option>
-                        <option value={600}>400</option>
-                        <option value={700}>500</option>
-                        <option value={800}>600</option>
-                        <option value={900}>700</option>
-                    </select>
-                </label>
-                <button>Buscar</button>
-            </form>
+            <div>
+                <form onSubmit={handleSubmit}>
+                    <input name='ciudad' placeholder={city ? city : cityUrl} value={city} onChange={e => setCity(e.target.value)} />
+                    <input name='provincia' placeholder='Provincia...' value={provincia} onChange={e => setProvincia(e.target.value)} />
+                    <label>
+                        Baños
+                        <select name='banos' value={nBanos} onChange={e => setNBanos(e.target.value)}>
+                            <option value='' >Baños</option>
+                            <option value={1} >1</option>
+                            <option value={2} >2</option>
+                            <option value={3} >3 o más</option>
+                        </select>
+                    </label>
+                    <label>
+                        Habitaciones
+                        <select name='habitaciones' value={nHabitaciones} onChange={e => setNHabitaciones(e.target.value)}>
+                            <option value='' >Habitaciones</option>
+                            <option value={1} >1</option>
+                            <option value={2} >2</option>
+                            <option value={3} >3</option>
+                            <option value={4} >4 o más</option>
+                        </select>
+                    </label>
+                    <label>
+                        Metros cuadrados
+                        <select name='m2' value={m2} onChange={e => setM2(e.target.value)}>
+                            <option value='' hidden>m2</option>
+                            <option value={50} >50</option>
+                            <option value={70} >70</option>
+                            <option value={90} >90</option>
+                            <option value={110} >110 o más</option>
+                        </select>
+                    </label>
+                    <label>
+                        Precio
+                        <select name='' value={precio1} onChange={e => setPrecio1(e.target.value)}>
+                            <option value='' hidden>Min</option>
+                            <option value={200}>400</option>
+                            <option value={300}>500</option>
+                            <option value={400}>600</option>
+                            <option value={500}>700</option>
+                        </select>
+                        <select value={precio2} onChange={e => setPrecio2(e.target.value)}>
+                            <option value='' hidden>Max</option>
+                            <option value={700}>700</option>
+                            <option value={800}>800</option>
+                            <option value={900}>900</option>
+                            <option value={1000}>1000</option>
+                        </select>
+                    </label>
+                    <label>
+                        Ascensor
+                        <input type='checkbox' name='ascensor' checked={ascensor} onChange={e => setAscensor(e.target.checked)} />
+                        Garaje
+                        <input type='checkbox' name='garaje' checked={garaje} onChange={e => setGaraje(e.target.checked)} />
+                        Balcón
+                        <input type='checkbox' name='balcon' checked={balcon} onChange={e => setBalcon(e.target.checked)} />
+                        Jardín
+                        <input type='checkbox' name='jardin' checked={jardin} onChange={e => setJardin(e.target.checked)} />
+                    </label>
+                    <button>Buscar</button>
+                </form>
+                <div>
+                    <button onClick={handleReset}>Borrar</button>
+                </div>
+            </div>
             <div>
                 <ul>
-                    {firstResults && firstResults.map(result =>
+                    {dataResults && dataResults.map(result =>
                         <li key={result.id}>
-                            <div>
-                                {result.ciudad}
-                            </div>
-                            <div>
-                                {result.direccion}
-                            </div>
+                            <div>{result.ciudad}</div>
+                            <div>{result.provincia}</div>
+                            <div>{result.direccion}</div>
+                        </li>
+                    )}
+                </ul>
+            </div>
+            <div>
+                <ul>
+                    {!dataResults && firstResults && firstResults.map(result =>
+                        <li key={result.id}>
+                            <div>{result.ciudad}</div>
+                            <div>{result.provincia}</div>
+                            <div>{result.direccion}</div>
                         </li>
                     )}
                 </ul>
