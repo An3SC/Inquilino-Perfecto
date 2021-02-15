79d7fda746c4bc9b6e37bcb40594ac4b2538e430
An3SC <asierracardalda@outlook.es>
2021-02-15 12:11:31 +0100

    Añadidos campos de busqueda por fecha al FirstSearch

--- inquilino-front/src/Inicio/FirstSearch.js
+++ inquilino-front/src/Inicio/FirstSearch.js
@@ -3,6 +3,8 @@ import { useHistory } from "react-router-dom"
 
 function FirstSearch() {
     const [cityUrl, setCityUrl] = useState('')
+    const [fechaEntrada, setFechaEntrada] = useState()
+    const [fechaSalida, setFechaSalida] = useState()
 
     let history = useHistory()
 
@@ -17,6 +19,14 @@ function FirstSearch() {
                 <input type='text' placeholder='¿En qué ciudad buscas?'
                     value={cityUrl} onChange={e => setCityUrl(e.target.value)}
                 />
+                <label>
+                    Fecha de entrada
+                    <input type='date' value={fechaEntrada} onChange={e => setFechaEntrada(e.target.value)} />
+                </label>
+                <label>
+                    Fecha de salida
+                    <input type='date' value={fechaSalida} onChange={e => setFechaSalida(e.target.value)} />
+                </label>
                 <button>Buscar</button>
             </form>
         </div>
