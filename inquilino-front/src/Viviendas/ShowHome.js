f68d782d787584f8b0b06b05588cc6c4fe7fe157
An3SC <asierracardalda@outlook.es>
2021-02-16 12:03:50 +0100

    Implementada la reserva de pisos

--- inquilino-front/src/Viviendas/ShowHome.js
+++ inquilino-front/src/Viviendas/ShowHome.js
@@ -1,11 +1,33 @@
+import { useState } from "react"
+import { useSelector } from "react-redux"
 import { useParams } from "react-router-dom"
 import useFetch from "../useFetch"
 
 function ShowHome() {
     const { id } = useParams()
 
-    const vivienda = useFetch(`http://localhost:9999/vivienda/` + `${id}`) || []
-    console.log(vivienda)
+    const login = useSelector(s => s.login)
+
+    const [fecha_entrada, setFechaEntrada] = useState('')
+    const [fecha_salida, setFechaSalida] = useState('')
+
+    const vivienda = useFetch(`http://localhost:9999/vivienda/${id}`) || []
+
+    const handleBooking = e => {
+        e.preventDefault()
+        try {
+            const ret = fetch(`http://localhost:9999/vivienda/${id}/reserva`, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': login.token
+                },
+                body: JSON.stringify({ fecha_entrada, fecha_salida }),
+                method: 'POST'
+            })
+        } catch (e) {
+            console.log(e)
+        }
+    }
 
     return (
         <div className='showHomeContainer'>
@@ -16,6 +38,15 @@ function ShowHome() {
                     <div>{v.direccion}</div>
                 </div>
             )}
+            <form onSubmit={handleBooking}>
+                <label>
+                    Fecha de entrada
+                    <input type='date' name='fechaEntrada' value={fecha_entrada} onChange={e => setFechaEntrada(e.target.value)} />
+                    Fecha de salida
+                    <input type='date' name='fechaSalida' value={fecha_salida} onChange={e => setFechaSalida(e.target.value)} />
+                </label>
+                <button>Reservar</button>
+            </form>
         </div>
     )
 }
