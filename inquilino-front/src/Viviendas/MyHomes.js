e94d8d3fbdde70a401bcd49d5f38a99f9fa66ceb
An3SC <asierracardalda@outlook.es>
2021-02-17 21:57:27 +0100

    Lista reestructurada

--- inquilino-front/src/Viviendas/MyHomes.js
+++ inquilino-front/src/Viviendas/MyHomes.js
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom"
+import { Link, useParams } from "react-router-dom"
 import useFetch from "../useFetch"
 
 function MyHomes() {
@@ -9,11 +9,13 @@ function MyHomes() {
     return (
         <div className='myHomesContainer'>
             {homesData && homesData.map(h =>
-                <ul>
-                    <li>{h.provincia}</li>
-                    <li>{h.ciudad}</li>
-                    <li>{h.direccion}</li>
-                </ul>
+                <Link key={h.id} to={`/home/${h.id}`}>
+                    <ul>
+                        <li>{h.provincia}</li>
+                        <li>{h.ciudad}</li>
+                        <li>{h.direccion}</li>
+                    </ul>
+                </Link>
             )}
         </div>
     )
