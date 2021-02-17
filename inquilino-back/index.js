46573a22e5edf3910a38e5e6a60c57aae9f5005d
An3SC <asierracardalda@outlook.es>
2021-02-17 21:47:49 +0100

    Cambiada la path a las imagenes

--- inquilino-back/index.js
+++ inquilino-back/index.js
@@ -60,7 +60,7 @@ app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(fileUpload())
-app.use('/imagen', express.static(__dirname + '/imagen'))
+app.use('/images', express.static(__dirname + '/images'))
 
 const DEFAULT_PORT = 3333
 
