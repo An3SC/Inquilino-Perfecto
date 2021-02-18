b0c7fd9a51022c7e595666cf69eeef7cef78cfe7
An3SC <asierracardalda@outlook.es>
2021-02-18 09:25:51 +0100

    Implementado el local storage en index.js

--- inquilino-front/src/index.js
+++ inquilino-front/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom'
-import { createStore, combineReducers } from 'redux'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux';
 import loginReducer from './Store/loginReducer';
 import registerReducer from './Store/registerReducer'
@@ -16,7 +16,14 @@ const rootReducer = combineReducers({
   register: registerReducer
 })
 
-const store = createStore(rootReducer)
+const localStorageMiddleware = store => next => action => {
+  let result = next(action)
+  localStorage.setItem('session', JSON.stringify(store.getState()))
+  return result
+}
+const saved = localStorage.getItem('session')
+const initialStore = saved ? JSON.parse(saved) : undefined
+const store = createStore(rootReducer, initialStore, applyMiddleware(localStorageMiddleware))
 
 ReactDOM.render(
   <React.StrictMode>
