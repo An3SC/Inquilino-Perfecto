21f8559cc796e16936f660bd233980501c5c349a
An3SC <asierracardalda@outlook.es>
2021-02-19 15:12:42 +0100

    Intentos de sacar el id de reserva nada mas crearla

--- inquilino-front/src/Bookings/Reservar.js
+++ inquilino-front/src/Bookings/Reservar.js
@@ -1,19 +1,24 @@
 import { useState } from "react"
-import { useSelector } from "react-redux"
-import { useParams } from "react-router-dom"
+import { useDispatch, useSelector } from "react-redux"
+import { useHistory, useParams } from "react-router-dom"
 
 function Reservar() {
     const { id } = useParams()
 
     const login = useSelector(s => s.login)
 
+    const booking = useSelector(s => s.booking)
+    const dispatch = useDispatch()
+
     const [fecha_entrada, setFechaEntrada] = useState('')
     const [fecha_salida, setFechaSalida] = useState('')
 
-    const handleBooking = e => {
+    // const history = useHistory()
+
+    const handleBooking = async e => {
         e.preventDefault()
         try {
-            fetch(`http://localhost:9999/vivienda/${id}/reserva`, {
+            const ret = await fetch(`http://localhost:9999/vivienda/${id}/reserva`, {
                 headers: {
                     'Content-Type': 'application/json',
                     'Authorization': login.token
@@ -21,6 +26,14 @@ function Reservar() {
                 body: JSON.stringify({ fecha_entrada, fecha_salida }),
                 method: 'POST'
             })
+            console.log(ret)
+            const data = await ret.json()
+            dispatch({ type: 'booking', data })
+
+            console.log(booking.resultId)
+            if (ret.ok) {
+                // history.push('/booking/:id')
+            }
         } catch (e) {
             console.log(e)
         }
