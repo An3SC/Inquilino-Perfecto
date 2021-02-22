2dd2ecd2bf3ed0d76edbe7e3ae4f1792ece87d0f
An3SC <asierracardalda@outlook.es>
2021-02-22 13:26:34 +0100

    Pruebas con Ratings y Valorar

--- inquilino-front/src/Utils/Rating.js
+++ inquilino-front/src/Utils/Rating.js
@@ -8,13 +8,6 @@ function Rating({ value }) {
             <span>{value >= 4 ? <div className='fullStar' /> : <div className='emptyStar' />}</span>
             <span>{value >= 5 ? <div className='fullStar' /> : <div className='emptyStar' />}</span>
         </div>
-        //     <div>
-        //         <span onClick={e => setRating(1)}>{rating >= 1 ? '⭐' : '✰'}</span>
-        //         <span onClick={e => setRating(2)}>{rating >= 2 ? '⭐' : '✰'}</span>
-        //         <span onClick={e => setRating(3)}>{rating >= 3 ? '⭐' : '✰'}</span>
-        //         <span onClick={e => setRating(4)}>{rating >= 4 ? '⭐' : '✰'}</span>
-        //         <span onClick={e => setRating(5)}>{rating >= 5 ? '⭐' : '✰'}</span>
-        //     </div>
     )
 }
 
