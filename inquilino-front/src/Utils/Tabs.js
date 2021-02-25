4d5178ce10f3012267569cde5488395148b3afcd
An3SC <asierracardalda@outlook.es>
2021-02-25 13:34:50 +0100

    Implementados tabs diferenciados para same y notsame user

--- inquilino-front/src/Utils/Tabs.js
+++ inquilino-front/src/Utils/Tabs.js
@@ -1,20 +1,23 @@
 import { useState } from 'react'
 import { useSelector } from 'react-redux'
 import { NavLink } from 'react-router-dom'
+import useFetch from '../useFetch'
 import './Utils.css'
 
-function Tabs() {
+function Tabs({ id }) {
     const [active,] = useState()
+
+    const data = useFetch(`http://localhost:9999/usuario/${id}`)
 
     const login = useSelector(s => s.login)
 
-    const tabList = ['Viviendas', 'Reservas']
+    const tabList = data ? (data[0].id === login.id ? ['Viviendas', 'Reservas'] : ['Viviendas']) : []
 
     return (
         <div className='tabs '>
-            {tabList.map(tab =>
-                <NavLink to={`/user/${login && login.id}/${tab}`} activeClassName={active}>{tab}</NavLink>
-            )}
+            <NavLink to={`/user/${data && data[0].id}/${tabList[0]}`} activeClassName={active}>{tabList[0]}</NavLink>
+            {data && data[0].id === login.id &&
+                <NavLink to={`/user/${login && login.id}/${tabList[1]}`} activeClassName={active}>{tabList[1]}</NavLink>}
         </div>
     )
 }
