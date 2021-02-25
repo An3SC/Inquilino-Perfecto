e8b470464396e2d49fc990d236a476f52d4994c7
An3SC <asierracardalda@outlook.es>
2021-02-25 13:30:13 +0100

    Añadido campo estado a la peticion getBooking

--- inquilino-back/db/mysql.js
+++ inquilino-back/db/mysql.js
@@ -285,6 +285,7 @@ const getBooking = async (id) => {
                     r.fecha_entrada,
                     r.fecha_salida,
                     r.score_piso,
+                    r.estado,
                     r.score_usuario
                     from reserva r join piso p on r.id_piso =p.id where r.id_reserva = ?`
     const params = [id]
